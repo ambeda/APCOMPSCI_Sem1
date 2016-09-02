fbd574d469d587b631190f9b4e8deec373b73c44
ambeda <taylahrei@gmail.com>
2016-09-02 14:03:39 -0700

    Aded Files

---
+++ APLesson_01/ASCII1.java
@@ -0,0 +1,13 @@
+public class ASCII1
+{
+		public static void main(String[]args)
+		{
+			System.out.println("              __          __");
+			System.out.println("             /   \\~~~/   \\");
+			System.out.println("   ,------(        ..       )");
+			System.out.println("  /          \\__        __/");
+			System.out.println(" /|                (\\     |(");
+			System.out.println("^ \\    /___\\   / \\   |");
+			System.out.println("    |__|      |_|-  \"");
+		}
+}
---
+++ APLesson_01/ASCII2.java
@@ -0,0 +1,14 @@
+public class ASCII2
+{
+		public static void main(String[]args)
+		{
+			System.out.println("      ,\\");
+			System.out.println(" __/_\\__");
+			System.out.println(" , (( \'\' )\\V/");
+			System.out.println("  \\_/  \\_|");
+			System.out.println("    /__\\ |");
+			System.out.println("   _/  |_|");
+			
+
+		}
+}
