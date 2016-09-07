7b35c80f3a1fbde323183184eebbe8cb032241cd
ambeda <taylahrei@gmail.com>
2016-09-07 14:31:37 -0700

    Lesson_02 Files

---
+++ APLesson_02/Lesson_02.java
@@ -0,0 +1,12 @@
+public class Lesson_02
+{
+	public static void main(String[]args)
+	{
+		String name = "pretty cool";
+		
+		String first = "dank";
+		String second = " memes";
+		
+		System.out.println(first + second);
+	}
+}
