5c04021714404fed3ecb179316a81411383e9e80
ambeda <taylahrei@gmail.com>
2016-10-13 14:32:14 -0700

    Discount & GPA lab

--- APLesson_05/Lab_05Discount.java
+++ APLesson_05/Lab_05Discount.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 public class Lab_05Discount
 {
 	public static void main(String[]args)
 	{
-			Lab_05Discount form = new Lab_05Discount();
 		Scanner keyboard = new Scanner(System.in);
+		
 		System.out.println ("Please enter item 1");
 		String word1 = keyboard.nextLine();
 		System.out.println ("Please enter the price");
@@ -21,31 +22,39 @@ public class Lab_05Discount
 		System.out.println ("Please enter the price");
 		double number3 = keyboard.nextDouble();
 		keyboard.nextLine();
-		System.out.println ("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
-		form.format(word1, number1);
-		form.format(word2, number2);
-		form.format(word3, number3);
 		
-		double subtotal = (number1 + number2 + number3);
-		System.out.printf ("\n\n* %20s   . . . . . . . . . . %10.2f\n", "Subtotal:", subtotal);
+		System.out.println ("Please enter item 4");
+		String word4 = keyboard.nextLine();
+		System.out.println ("Please enter the price");
+		double number4 = keyboard.nextDouble();
+		keyboard.nextLine();
 		
-		double tax = (subtotal * 0.08);
-		System.out.printf ("* %20s   . . . . . . . . . . %10.2f\n", "Tax:", tax);
+		double subtotal = number1 + number2 + number3 + number4;
+		double discount = discount(subtotal);
+		double tax = subtotal * 0.08;
+		double total = subtotal-discount + tax;
 		
-		double total = (subtotal + tax);
-		System.out.printf ("* %20s   . . . . . . . . . . %10.2f\n", "Total:", total);
+		System.out.println ("<<<<<<<<<< Receipt >>>>>>>>>>");
+		format(word1, number1);
+		format(word2, number2);
+		format(word3, number3);
+		format(word4, number4);
 		
-		System.out.println ("_______________________________________________________________\n* Thank you for your support *");
+		format("Subtotal: ", subtotal);
+		format("Tax: ", tax);
+		format("Total: ", total);
 	}
-	
-	
-	public static void discount(int number1, int number2, int number3, boolean discount)
+	public static double discount(double subtotal)
 	{
-		
+		if (subtotal >= 2000)
+		{
+			return subtotal * 0.15;
+		}
+		return 0.0;
 	}
 	
 	
-	public void format(String word, double number)
+	public static void format(String word, double number)
 	{
 		System.out.printf("* %20s   . . . . . . . . . . %10.2f\n", word, number);
 	}
