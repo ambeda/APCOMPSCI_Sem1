6111b028da80749268104b3526d4cffd467f5c3f
ambeda <taylahrei@gmail.com>
2016-10-19 14:35:12 -0700

    GPA lab

---
+++ APLesson_05/Lab_05GPA.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Lab_05GPA
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		
+		
+		System.out.println("Subject 1 Grade:");
+		String g1 = kb.next(); 
+		System.out.println("Subject 2 Grade:");
+		String g2 = kb.next();
+		System.out.println("Subject 3 Grade:");
+		String g3 = kb.next(); 
+		System.out.println("Subject 4 Grade:");
+		String g4 = kb.next(); 
+		System.out.println("Subject 5 Grade:");
+		String g5 = kb.next(); 
+		System.out.println("Subject 6 Grade:");
+		String g6 = kb.next(); 
+		System.out.println("Subject 7 Grade:");
+		String g7 = kb.next(); 
+		
+		
+	}
+	
+	public static double calcPoints(String gpa)
+	{
+		if (gpa.equals("A"))
+			// blah
+		else if (gpa.equals)
+			
+		
+		return 0.0; 
+		
+	}
+}
