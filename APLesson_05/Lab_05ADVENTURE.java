f3297d58b062bd8aa4798489fa828fa8017a1d3f
ambeda <taylahrei@gmail.com>
2016-10-25 14:34:53 -0700

    adventure

---
+++ APLesson_05/Lab_05ADVENTURE.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Lab_05ADVENTURE
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println ("You are going on a quest to slay a monster.\nIn this land, there are two different kinds of monster to slay- Dragons and Goblins.\nWhich would you like to slay?\n(A = Dragons, B = Goblins)");
+		String choice1 = kb.next();
+		
+	}
+}
